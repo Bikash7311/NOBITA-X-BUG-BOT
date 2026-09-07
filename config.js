1afe8e3ca80da9b2875b9ba5e7d7a0a053d75118
Bikash7311 <Bikash7311@users.noreply.github.com>
2026-09-07 13:33:32 +0000

    Initial upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8646207258:AAGVjSz_lGWiymIL1_DmeokbLokKz6qaR3I', // Bot Token Here 
+    
+    S7: 'Nobita Bug Bot', // Owner, Name
+    
+    adminId: '6132146801', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/nobitabanxunban', // TG channel link
+    
+    
+    
+    bot: 'Nobita X Bug Bot', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
