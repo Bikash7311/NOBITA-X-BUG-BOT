999b16139b6d34cdf4974f6f5bdf9740adba1cee
Bikash7311 <161233363+Bikash7311@users.noreply.github.com>
2026-09-13 13:31:46 +0530

    config.js

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
-
+```js
 module.exports = {
-    mainToken: '8646207258:AAGVjSz_lGWiymIL1_DmeokbLokKz6qaR3I', // Bot Token Here 
-    
-    S7: 'Nobita Bug Bot', // Owner, Name
-    
-    adminId: '6132146801', // Owner Chat ID ( control /address, /delresell )
-    
-    channel: 'https://t.me/nobitabanxunban', // TG channel link
-    
-    
-    
-    bot: 'Nobita X Bug Bot', // Bot name 
-    
-    logo: './SY/Loves.jpg' // Bot logo path or URL
+    mainToken: process.env.BOT_TOKEN,
+
+    S7: 'Nobita Bug Bot',
+
+    adminId: '6132146801',
+
+    channel: 'https://t.me/nobitabanxunban',
+
+    bot: 'Nobita X Bug Bot',
+
+    logo: './SY/Loves.jpg'
 };
+```
